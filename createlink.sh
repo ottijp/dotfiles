dd0e6e643038cc236d2119edd6002b37dfc4a246
ottijp <ottijp@users.noreply.github.com>
2015-01-21 22:43:49 +0900

    bugfix

--- createlink.sh
+++ createlink.sh
@@ -3,5 +3,5 @@
 ln -snf ~/dotfiles/.vim ~/.vim
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.xvimrc ~/.xvimrc
-ln -snf ~/dotfiles/.vimperatorrc /.vimperatorrc
+ln -snf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 
