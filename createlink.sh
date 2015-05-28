e8645f6f480b8e3b65a2be5e7ce86fa42b24a929
ottijp <ottijp@users.noreply.github.com>
2015-05-29 08:31:25 +0900

    add tmux config

--- createlink.sh
+++ createlink.sh
@@ -4,4 +4,5 @@ ln -snf ~/dotfiles/.vim ~/.vim
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -snf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
+ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
