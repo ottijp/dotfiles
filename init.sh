cdd044f2fff9509a02503c631c461f49e8cc9a8c
ottijp <ottijp@users.noreply.github.com>
2016-04-04 18:15:04 +0900

    [Add] .gvimrc

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
+ln -snf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -snf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -snf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
