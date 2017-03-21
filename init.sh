3c6483a96942288314d4f49f2c3974d160887dce
Satoshi SAKAO <ottijp@users.noreply.github.com>
2017-03-22 07:58:09 +0900

    added .ideavimrc

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 ln -snf ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -snf ~/dotfiles/.xvimrc ~/.xvimrc
+ln -snf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -snf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -snf ~/dotfiles/.gitconfig ~/.gitconfig
